6cb71c735c9d99a8ce406fd737b86e274f92a6d7
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-04-20 06:41:53 +0200

    added netsh scripts

---
+++ windows/low.bat
@@ -0,0 +1,28 @@
+@echo off
+
+:isAdmin
+:: Check for admin rights
+NET SESSION >nul 2>&1
+if %errorLevel% == 0 (
+    echo protecting the device...
+) else (
+    echo Requesting administrator permissions...
+    :: Re-launch as admin
+    PowerShell -Command "Start-Process cmd -ArgumentList '/c %~dpnx0' -Verb RunAs"
+    exit /b
+)
+
+:netshScript
+for /f "tokens=5 delims= " %%i in ('netsh interface ip show interfaces ^| findstr "connected"') do (
+
+    @rem setting primary dns
+    netsh interface ip set dns %%i static 15.184.147.40
+
+    @rem setting secondary dns
+    netsh interface ip add dns name="%%i" 15.184.182.221 index=2
+
+    echo a new interface is protected!!
+)
+
+echo everything is ok now! you can exit
+pause
