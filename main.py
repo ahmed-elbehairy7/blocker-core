51a58c92fdfebd8f2aa5ddb387d2c04467c5f459
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-04-24 17:09:14 +0200

    added enable-youtube script

--- main.py
+++ main.py
@@ -16,24 +16,30 @@ DNS = {
 
 SCRIPTS = [
     {
-        "dir" : "windows",
+        "os" : "windows",
+        "script_name": "protect",
+        "ext": "bat"
+    },
+    {
+        "os": "windows",
+        "script_name": "enable-youtube",
         "ext": "bat"
     }
 ]
 
 def main():
     
-    for os in SCRIPTS:
-        with open(join(os["dir"], 'protect.' + os['ext'])) as file:
+    for script in SCRIPTS:
+        with open(join(script["os"], script['script_name'] + '.' + script['ext'])) as file:
             text = file.read()
             for level in ["high", "low"]:
-                generate_file(level, text, os["dir"], os["ext"])
+                generate_file(level, text, script)
 
 def process_text(text: str, level: str):
     return text.replace("{{primary-dns}}", DNS[level]['primary-dns']).replace("{{secondary-dns}}", DNS[level]['secondary-dns']).replace("{{doh}}", DNS[level]['doh'])
 
-def generate_file(level: str, text: str, dir: str, ext: str):
-    with open(join(dir, f"{level}.{ext}"), 'w') as file:
+def generate_file(level: str, text: str, script: str):
+    with open(join(script['os'], f"{script['script_name']}-{level}.{script['ext']}"), 'w') as file:
         file.write(process_text(text, level))
 
     
--- release.bat
+++ release.bat
@@ -1,1 +1,3 @@
-gh release create %1 --generate-notes .\windows\high.bat#windows/high.bat .\windows\low.bat#windows/low.bat
+gh release create %1 --generate-notes \ @REM main command
+.\windows\protect-high.bat#windows/protect-high.bat .\windows\protect-low.bat#windows/protect-low.bat \ @REM adding windows protect script as assets
+.\windows\enable-youtube-high.bat#windows/enable-youtube-high.bat .\windows\enable-youtube-low.bat#windows/enable-youtube-low.bat   @REM Adding windows enable youtube scripts as assets
