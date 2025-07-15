d98c8c9dac65b0cb1b8fe198bf37cc030a44c6c2
‪Ahmed Elbehairy‬‏ <138185767+ahmed-elbehairy7@users.noreply.github.com>
2025-07-15 16:34:49 +0300

    Create deactivate.bat

---
+++ windows/deactivate.bat
@@ -0,0 +1,63 @@
+@echo off
+
+:isAdmin
+:: Check for admin rights
+NET SESSION >nul 2>&1
+if %errorlevel% == 0 (
+    echo protecting the device...
+) else (
+    echo Requesting administrator permissions...
+    :: Re-launch as admin
+    PowerShell -Command "Start-Process cmd -ArgumentList '/c %~dpnx0' -Verb RunAs"
+    exit /b
+)
+
+set /a layer=1
+
+@rem chrome script from the registry editor
+:regEditChromeScript
+set /a layer=%layer%+1
+
+echo applying layer %layer% protection
+set k=HKLM\SOFTWARE\Policies\Google\Chrome
+reg query "%k%" >nul 2>&1 || (
+    reg add "%k%" /f
+)
+reg add "%k%" /v "DnsOverHttpsMode" /t REG_SZ /d "automatic" /f
+reg add "%k%" /v "DnsOverHttpsTemplates" /t REG_SZ /d "https://family-filter-dns.cleanbrowsing.org" /f
+echo protection layer %layer% is applied successfully
+
+
+@rem brave script from the registry editor
+:regEditBraveScript
+set /a layer=%layer%+1
+
+echo applying layer %layer% protection
+set k=HKLM\Software\Policies\BraveSoftware\Brave
+reg query "%k%" >nul 2>&1 || (
+    reg add "%k%" /f
+)
+reg add "%k%" /v "DnsOverHttpsMode" /t REG_SZ /d "automatic" /f
+reg add "%k%" /v "DnsOverHttpsTemplates" /t REG_SZ /d "https://family-filter-dns.cleanbrowsing.org" /f
+echo protection layer %layer% is applied successfully
+
+
+@rem netsh script
+:netshScript
+setlocal enableDelayedExpansion
+
+
+for /f "tokens=5 delims= " %%i in ('netsh interface ip show interfaces ^| findstr "connected"') do (
+
+    call set /a layer=layer+1
+    
+    netsh interface ip set dns %%i static 185.228.168.168
+
+    netsh interface ip add dns name="%%i" 185.228.169.168 index=2
+
+    echo layer !layer! is applied successfully
+)
+echo.
+echo.
+echo %layer% layers of protection are applied on the device successfully! you can now exit
+pause
