8106e88a2bc43167f31eb14d35da59234c23b01e
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2025-07-17 15:58:00 +0300

    saving changes

--- windows/protect.bat
+++ windows/protect.bat
@@ -2,22 +2,24 @@
 
 :: Main script for managing blocker functionalities
 
-if "%~1"=="" (
-    goto :show_usage
-) else if /i "%~1"=="add_domain" (
-    goto :isAdmin
-) else if /i "%~1"=="apply" (
-    goto :isAdmin
-) else if /i "%~1"=="deactivate" (
-    goto :isAdmin
-) else if /i "%~1"=="enable_youtube" (
-    goto :isAdmin
-) else (
-    echo Error: Invalid subcommand "%~1"
-    goto :show_usage
-)
+:start
 
+    if "%~1"=="" (
+        goto :show_usage
+    ) else if /i "%~1"=="add_domain" (
+        goto :isAdmin
+    ) else if /i "%~1"=="apply" (
+        goto :isAdmin
+    ) else if /i "%~1"=="deactivate" (
+        goto :isAdmin
+    ) else if /i "%~1"=="enable_youtube" (
+        goto :isAdmin
+    ) else (
+        echo Error: Invalid subcommand "%~1"
+        goto :show_usage
+    )
 
+:: --- show usage ---
 :show_usage
     echo.
     echo Usage: %~nx0 [subcommand]
@@ -31,64 +33,67 @@ if "%~1"=="" (
     echo.
     goto :eof
 
+:: --- check for admin permissions ---
 :isAdmin
 
-:: Check for admin rights
-NET SESSION >nul 2>&1
-if %errorlevel% == 0 (
-    echo running protect...
-) else (
-    echo Requesting administrator permissions...
-    :: Re-launch as admin
-    PowerShell -Command "Start-Process cmd -ArgumentList '/c %~dpnx0 %*' -Verb RunAs"
-    exit /b
-)
+    :: Check for admin rights
+    NET SESSION >nul 2>&1
+    if %errorlevel% == 0 (
+        echo running protect...
+    ) else (
+        echo Requesting administrator permissions...
+        :: Re-launch as admin
+        PowerShell -Command "Start-Process cmd -ArgumentList '/c %~dpnx0 %*' -Verb RunAs"
+        exit /b
+    )
 
-set "subcommand=%~1"
-shift
+    set "subcommand=%~1"
+    shift
 
-if /i "%subcommand%"=="add_domain" (
-    call :add_domain_func %*
-) else if /i "%subcommand%"=="deactivate" (
-    call :deactivate_func %*
-) else if /i "%subcommand%"=="apply" (
-    call :apply_func %*
-) else if /i "%subcommand%"=="enable_youtube" (
-    call :enable_youtube_func %*
-) else (
-    echo Error: Unknown subcommand "%subcommand%".
-    echo Type %~nx0 for usage.
-)
+    if /i "%subcommand%"=="add_domain" (
+        call :add_domain_func %*
+    ) else if /i "%subcommand%"=="deactivate" (
+        call :deactivate_func %*
+    ) else if /i "%subcommand%"=="apply" (
+        call :apply_func %*
+    ) else if /i "%subcommand%"=="enable_youtube" (
+        call :enable_youtube_func %*
+    ) else (
+        echo Error: Unknown subcommand "%subcommand%".
+        echo Type %~nx0 for usage.
+    )
 
-goto :eof
+    goto :eof
 
 :: --- Subcommand Functions ---
 
+:: --- add domain subcommand ---
 :add_domain_func
 
     
     if "%~2"=="" (
         echo Error: Text to add not provided.
-        goto :error
+        exit /b 1
     )
 
     :: Assign arguments to variables
     set "file_path=%SystemRoot%\System32\drivers\etc\hosts"
     set "text_to_add=%~2 %~3"
 
-    echo %text_to_add%
-
+    echo adding %text_to_add%
     :: Check if the text already exists in the file
     findstr /x /c:"%text_to_add%" "%file_path%" >nul
     if %ERRORLEVEL% equ 0 (
-        echo Text '%text_to_add%' already exists in the hosts file.
-        goto :error
+        echo line '%text_to_add%' already exists in the hosts file.
+        exit /b 1
     ) else (
         :: Append the text to the file
         echo %text_to_add%>>"%file_path%"
     )
-    goto :finish
+    
+    exit /b 0
 
+:: --- deactivate subcommand ---
 :deactivate_func
 
     set "DnsOverHttps=family-filter-dns.cleanbrowsing.org"
@@ -99,8 +104,9 @@ goto :eof
 
     call :apply_dns
 
-    goto :finish
+    exit /b 0
 
+:: --- apply protection subommand ---
 :apply_func
   
     set /a layer=1
@@ -109,12 +115,12 @@ goto :eof
 
     if /i not "%~2"=="high" if /i not "%~2"=="low" (
         echo Error: Level must be either "high" or "low"
-        goto :error
+        exit /b 1
     )
 
     if /i not "%~3"=="true" if /i not "%~3"=="false" (
         echo Error: YouTube setting must be either "true" or "false"
-        goto :error
+        exit /b 1
     )
 
     set "level=%~2"
@@ -137,8 +143,11 @@ goto :eof
 
     call :hostsFileScript
 
-    goto :finish
+    exit /b 0
 
+:: --- common used functions ---
+
+:: --- hosts file script ---
 :hostsFileScript
     echo %act% layer %layer% protection
     set file=C:\Windows\System32\drivers\etc\hosts
@@ -356,7 +365,9 @@ goto :eof
         echo protection layer %layer% is %past_act% successfully
     )
 
+    exit /b 0
 
+:: --- apply dns ---
 :apply_dns
 
     @rem chrome script from the registry editor
@@ -385,7 +396,7 @@ goto :eof
         reg add "%k%" /f
     )
     reg add "%k%" /v "DnsOverHttpsMode" /t REG_SZ /d "automatic" /f
-    reg add "%k%" /v "DnsOverHttpsTemplates" /t REG_SZ /d "https://family-filter-dns.cleanbrowsing.org" /f
+    reg add "%k%" /v "DnsOverHttpsTemplates" /t REG_SZ /d "https://%DnsOverHttps%" /f
     echo protection layer %layer% is %past_act% successfully
 
 
@@ -406,13 +417,7 @@ goto :eof
     )
     echo.
     echo.
-    echo %layer% layers of protection are %past_act% on the device successfully! you can now exit
+    echo %layer% layers of protection are %past_act% on the device successfully!
 
-
-:error
-    pause
-    exit /b 1
-
-:finish
-    pause
     exit /b 0
+:: --- finish functions ---
